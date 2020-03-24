9302b6e000b3ce3fc66c7b9ff739415672c7eb17
Artem Sychov <artem.suchov@gmail.com>
2020-03-24 21:36:53 +0200

    add signin and signup component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.style.scss";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">SIGN IN</div>
+);
+
+export default SignInAndSignUpPage;
